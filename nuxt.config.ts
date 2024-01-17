7c1ee849606e526deb28815284b98c8b88c5383f
ningzi <45959659+ningzio@users.noreply.github.com>
2024-01-17 14:56:47 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
         vercel: {
             regions: ["cle1", "iad1", "pdx1", "sfo1", "sin1", "syd1", "hnd1", "kix1"]
         }
-    }
+    },
     routeRules: {
         '/': {
             prerender: true,
